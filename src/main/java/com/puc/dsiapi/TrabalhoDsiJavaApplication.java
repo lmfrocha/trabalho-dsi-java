984ca8d76f3b9b83f5b7bf0d9123f12d039b1c5a
lmfrocha <lucas.frocha@vli-logistica.com.br>
2019-03-16 20:09:26 -0300

    Subida de arquitetura do projeto

---
+++ src/main/java/com/puc/dsiapi/TrabalhoDsiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.puc.dsiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoDsiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoDsiJavaApplication.class, args);
+	}
+
+}
